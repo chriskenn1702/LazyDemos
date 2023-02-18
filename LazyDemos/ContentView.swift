3af3deaedd3c5ed12960434eec194f841e3f6990
Christopher Kennedy <kennednu@bc.edu>
2023-02-18 16:43:03 -0500

    Demo for learning LazyV and H Grid

--- LazyDemos/ContentView.swift
+++ LazyDemos/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var columns = [
+//        GridItem(.fixed(110)),
+//        GridItem(.fixed(150)),
+//        GridItem(.fixed(110))
+        GridItem(.flexible(maximum: 110)),
+        GridItem(.flexible(maximum: 200)),
+    ]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ScrollView{
+            LazyVGrid(columns: columns, alignment: .trailing) {
+                
+                ForEach(0..<1000) { i in
+                    let _ = print("Dang \(i)")
+                    RedAndCyanView()
+                }
+            }
         }
         .padding()
+        .ignoresSafeArea()
+    }
+}
+
+
+struct RedAndCyanView: View {
+    var body: some View {
+        ZStack{
+            Color(.red)
+            Rectangle()
+                .fill(.cyan)
+                .frame(width: 100, height: 100)
+        }
     }
 }
 
