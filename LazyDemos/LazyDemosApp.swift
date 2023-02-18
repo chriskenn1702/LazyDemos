b676c371a8516e530c0d19ed7c15b68de9850864
Christopher Kennedy <kennednu@bc.edu>
2023-02-18 16:12:09 -0500

    Initial Commit

---
+++ LazyDemos/LazyDemosApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyDemosApp.swift
+//  LazyDemos
+//
+//  Created by Christopher Kennedy on 2/18/23.
+//
+
+import SwiftUI
+
+@main
+struct LazyDemosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
